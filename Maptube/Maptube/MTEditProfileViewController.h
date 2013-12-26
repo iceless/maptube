3071b53bab0da23c124f40a883c48f1dc4968ff8
iceless <iceless.bing@gmail.com>
2013-12-26 18:29:58 +0800

    for debug

---
+++ Maptube/Maptube/MTEditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTEditProfileViewController.h
+//  Maptube
+//
+//  Created by Bing W on 12/26/13.
+//  Copyright (c) 2013 Bing W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTEditProfileViewController : UITableViewController
+
+@end
