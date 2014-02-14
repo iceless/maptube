9762f043583283124480193d7a4e28078642079e
BlueFeather <Vivian@ViviandeMacBook-Pro.local>
2014-02-14 14:50:57 +0800

    Add a View for add a place

---
+++ Maptube/Maptube/MTAddPlaceViewController.h
@@ -0,0 +1,19 @@
+//
+//  MTMapViewController.h
+//  Maptube
+//
+//  Created by Vivian on 14-2-14.
+//  Copyright (c) 2014年 Bing W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MTAddPlaceViewController : UIViewController<CLLocationManagerDelegate,MKMapViewDelegate,UITableViewDataSource,UITableViewDelegate>
+@property (strong, nonatomic)  IBOutlet MKMapView  *mapView;
+@property (strong, nonatomic) NSArray *nearbyPlaces;
+@property (strong,nonatomic )  IBOutlet UITableView *table;
+@property (strong, nonatomic)  CLLocationManager *locationManager;
+@property (assign, nonatomic)  CLLocationCoordinate2D  curLocation;
+
+@end
