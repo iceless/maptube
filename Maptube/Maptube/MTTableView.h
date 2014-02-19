171d4c51ac6008d7399ef9f2fc4e2b673bf43a5c
BlueFeather <Vivian@ViviandeMacBook-Pro.local>
2014-02-19 17:48:09 +0800

    Add a tableview can be scrolled

---
+++ Maptube/Maptube/MTTableView.h
@@ -0,0 +1,13 @@
+//
+//  MTTableView.h
+//  Maptube
+//
+//  Created by Vivian on 14-2-19.
+//  Copyright (c) 2014年 Bing W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTTableView : UITableView
+
+@end
