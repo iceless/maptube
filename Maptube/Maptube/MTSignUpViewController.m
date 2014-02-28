a1f54fee2efe4e077a03d86c264ebb4b9892182e
BlueFeather <Vivian@ViviandeMacBook-Pro.local>
2014-02-28 14:55:09 +0800

    finish the signup view

--- Maptube/Maptube/MTSignUpViewController.m
+++ Maptube/Maptube/MTSignUpViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MTSignUpViewController.h"
+#import <AVOSCloud/AVOSCloud.h>
 
 @interface MTSignUpViewController ()
 
@@ -30,7 +31,29 @@
 }
 
 -(IBAction)signUpClick:(id)sender{
+    
+    if(!self.accountField.text.length||!self.passwordField.text.length||!self.emailField.text.length){
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Missing Information", nil) message:NSLocalizedString(@"Make sure you fill out all of the information!", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil] show];
+        return;
+    }
+    else {
+    AVUser *user= [AVUser user];
+    user.username=self.accountField.text;
+    user.password=self.passwordField.text;
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"SignUp Success", nil) message:nil delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil] show];
+            [self dismissViewControllerAnimated:YES completion:NULL];
+            return;
 
+            
+        }else{
+            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"SignUp Error", nil) message:nil delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil] show];
+             return;
+        }
+    }];
+    }
 
 
 }
