0f7977af4a0b76211bb5521ca7e02e6d45d68a24
BlueFeather <Vivian@ViviandeMacBook-Pro.local>
2014-03-05 16:42:59 +0800

    add description display

--- Maptube/Maptube/MTProfileViewController.m
+++ Maptube/Maptube/MTProfileViewController.m
@@ -175,6 +175,9 @@
         label = (UILabel *)[cell viewWithTag:2];
         label.text = [NSString stringWithFormat:NSLocalizedString(@"%@", nil), [PFUser currentUser][@"location"]];
         
+        label = (UILabel *)[cell viewWithTag:3];
+        label.text = [NSString stringWithFormat:NSLocalizedString(@"%@", nil), [PFUser currentUser][@"description"]];
+        
         for(int i=11;i<=14;i++){
             imgv = (UIImageView *)[cell viewWithTag:i];
             imgv.layer.borderWidth = 1;
