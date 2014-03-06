b2370e7cdbd484be6a74034cede963e531af3ab9
BlueFeather <Vivian@ViviandeMacBook-Pro.local>
2014-03-06 15:22:45 +0800

    change map select style to none in the profile view

--- Maptube/Maptube/MTProfileViewController.m
+++ Maptube/Maptube/MTProfileViewController.m
@@ -154,6 +154,7 @@
     }
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     
     
