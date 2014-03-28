4d89fc1568af2519dd8cbf93d48c7192554dcd5d
BlueFeather <hw0729@gmail.com>
2014-03-28 16:30:51 +0800

    add edit place photo view
    https://tower.im/projects/7521914fda94470680a14eaf9563952a/todos/f02c142
    98b484f96bb060fbcc3ecf38f

---
+++ Maptube/Maptube/MTEditPlacePhotoViewController.h
@@ -0,0 +1,16 @@
+//
+//  MTEditPlacePhotoViewController.h
+//  Maptube
+//
+//  Created by Vivian on 14-3-28.
+//  Copyright (c) 2014年 Bing W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTEditPlacePhotoViewController : UIViewController
+@property (strong, nonatomic) NSArray *imageStrArray;
+@property (strong, nonatomic) NSString *placeName;
+@property (strong, nonatomic) NSString *location;
+
+@end
