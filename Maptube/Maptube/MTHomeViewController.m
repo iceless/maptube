ec23a260164fa12e1ee92e01c5664f50c03c2cfc
BlueFeather <hw0729@gmail.com>
2014-04-02 16:29:35 +0800

    write cell method
    https://tower.im/projects/7521914fda94470680a14eaf9563952a/todos/ee701e6
    b8cef42bdb0419c8fecdc9dcb

--- Maptube/Maptube/MTHomeViewController.m
+++ Maptube/Maptube/MTHomeViewController.m
@@ -9,6 +9,7 @@
 #import "MTHomeViewController.h"
 #import "MTMapCell.h"
 #import <AVOSCloud/AVOSCloud.h>
+#import "MTPlace.h"
 
 @interface MTHomeViewController ()
 
@@ -34,8 +35,13 @@
     self.table.delegate =self;
     self.table.dataSource = self;
     [self.view addSubview:self.table];
-    [self.table registerNib:[UINib nibWithNibName:@"MapCell" bundle:nil] forCellReuseIdentifier:@"PlaceSummaryCell"];
-     //[self updateMap];
+    [self.table registerNib:[UINib nibWithNibName:@"MapCell" bundle:nil] forCellReuseIdentifier:@"MapCell"];
+    self.mapList = [[NSMutableArray alloc]init];
+    if ([self.table respondsToSelector:@selector(setSeparatorInset:)]) {
+        [self.table setSeparatorInset:UIEdgeInsetsZero];
+    }
+
+     [self updateMap];
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,11 +55,13 @@
     //[query addDescendingOrder:CollectUser];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         NSLog(@"%@",error);
+        if(!error){
         for(int i=0;i<10;i++){
             [self.mapList addObject:[objects objectAtIndex:i]];
         }
     
         [self.table reloadData];
+        }
     }];
     
 }
@@ -62,18 +70,78 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return self.mapList.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    AVObject *mapObject = [self.mapList objectAtIndex:indexPath.row];
     
-    MTMapCell *cell = [[MTMapCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"PlaceSummaryCell"];
-    cell.nameLabel = [mapObject objectForKey:Title];
+    MTMapCell *cell = (MTMapCell *)[tableView dequeueReusableCellWithIdentifier:@"MapCell"];
+    
+    AVObject *mapObject = [self.mapList objectAtIndex:indexPath.row];
+    cell.nameLabel.text = [mapObject objectForKey:Title];
+    AVUser *user = [mapObject objectForKey:Author];
+    AVQuery *query =[AVQuery queryWithClassName:@"_User"];
+    [query whereKey:@"objectId" equalTo:[user objectForKey:@"objectId"]];
+    NSArray *uArray = [query findObjects];
+    
+    cell.authorLabel.text = [uArray[0] objectForKey:@"username"];
+    cell.iconImage.layer.masksToBounds = YES;
+    cell.iconImage.layer.cornerRadius =15;
+    
+    AVQuery *photoQuery = [AVQuery queryWithClassName:@"UserPhoto"];
+   
+    [photoQuery whereKey:@"user" equalTo:user];
+    NSArray *pArray = [photoQuery findObjects];
+    AVObject *photoObject = pArray[0];
+    AVFile *theImage = [photoObject objectForKey:@"imageFile"];
+    NSData *imageData = [theImage getData];
+    cell.iconImage.image = [UIImage imageWithData:imageData];
+    
+    AVRelation *collectRelation = [mapObject objectForKey:CollectUser];
+    NSArray *userCollectArray = [collectRelation.query findObjects];
+    cell.likeCountLabel.text = [NSString stringWithFormat:@"%d",userCollectArray.count];
+    PFRelation *relation = [mapObject relationforKey:Place];
+    NSArray *placeArray = [[relation query] findObjects];
+    cell.placeCountLabel.text = [NSString stringWithFormat:@"%d",placeArray.count];
+    
+    NSArray * array = [MTPlace convertPlaceArray:placeArray];
+    if(array.count!=0){
+        
+        CGRect placeRect = [MTPlace updateMemberPins:array];
+        CLLocationCoordinate2D coodinate = CLLocationCoordinate2DMake(placeRect.origin.x, placeRect.origin.y);
+        
+        //MTPlace *place = self.placeArray[0];
+        int distance = placeRect.size.width;
+        distance = MAX(1500,distance);
+        distance = MIN(distance, 15000000);
+        
+        //MKCoordinateRegion region=MKCoordinateRegionMakeWithDistance(coodinate,distance,distance);
+        
+        NSString *markStr = @"/";
+        for (MTPlace *place in array){
+            NSString *str = [NSString stringWithFormat:@"pin-s+48C(%f,%f),",place.coordinate.longitude,place.coordinate.latitude];
+            markStr = [markStr stringByAppendingString:str];
+        }
+        markStr = [markStr substringToIndex:([markStr length]-1)];
+        NSString *urlStr = [NSString stringWithFormat:@"%@%@%@/%f,%f,10/%.0fx%.0f.png",MapBoxPictureAPI,MapId,markStr,coodinate.longitude,coodinate.latitude,cell.mapImageView.frame.size.width,cell.mapImageView.frame.size.height];
+        [cell.mapImageView setImageWithURL:[NSURL URLWithString:urlStr]];
+        // [mapView setRegion:region animated:TRUE];
+        //[mapView addAnnotations:array];
+    }
+
+    
+   
     
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return 185;
+    
 }
 
 @end
