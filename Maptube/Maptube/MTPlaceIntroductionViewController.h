6a89c78fec5dfc9ae8e795d62e9bdf751909b956
BlueFeather <hw0729@gmail.com>
2014-04-17 15:04:32 +0800

    new place detail view

--- Maptube/Maptube/MTPlaceIntroductionViewController.h
+++ Maptube/Maptube/MTPlaceIntroductionViewController.h
@@ -8,13 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "FSVenue.h"
-
 #import "MTChooseBoardViewController.h"
+#import "MTMap.h"
+
+
 @interface MTPlaceIntroductionViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
 //@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
 @property (strong, nonatomic) UILabel *addressLabel;
 @property (strong, nonatomic) UILabel *distanceLabel;
-@property (strong, nonatomic) UIButton *mapButton;
 @property (strong, nonatomic) UIScrollView *scrollView;
 @property (strong, nonatomic) UIPageControl *pageControl;
 @property (strong, nonatomic) NSDictionary *placeData;
@@ -22,7 +23,9 @@
 @property (strong, nonatomic) UITableView *table;
 @property (strong, nonatomic) NSMutableArray *boardArray;
 @property (strong, nonatomic) NSMutableArray *imageUrlArray;
+@property (strong, nonatomic) MTMap *map;
+@property (strong, nonatomic) NSArray *mapPlaceArray;
 //@property (strong, nonatomic) IBOutlet MKMapView  *mapView;
 @property (strong, nonatomic) MTChooseBoardViewController *chooseBoardView;
--(id)initWithData:(NSDictionary *)dict AndVenue:(FSVenue *)venue;
+-(void)initData;
 @end
--- Maptube/Maptube/MTPlaceIntroductionViewController.m
+++ Maptube/Maptube/MTPlaceIntroductionViewController.m
@@ -8,10 +8,11 @@
 
 #import "MTPlaceIntroductionViewController.h"
 #import "MTMapViewController.h"
-
 #import <AVOSCloud/AVOSCloud.h>
 #import "MTPlace.h"
 #import  "MTEditPlacePhotoViewController.h"
+#import "MTPlaceImageViewController.h"
+#import "MTMapDetailViewController.h"
 
 @interface MTPlaceIntroductionViewController ()
 
@@ -28,58 +29,28 @@
     return self;
 }
 
--(id)initWithData:(NSDictionary *)dict AndVenue:(FSVenue *)venue{
-    self = [super initWithNibName:nil bundle:nil];
-    if (self) {
-        // Custom initialization
-        
-        
-        //self.view.backgroundColor = [UIColor grayColor];
-        //UIStoryboard * storyBoard  = [UIStoryboard
-         //                             storyboardWithName:@"Main" bundle:nil];
-        //self = [storyBoard instantiateViewControllerWithIdentifier:@"PlaceIntroduction"];
-      
-        self.placeData = dict;
-        self.venue = venue;
-        
-    }
-    return self;
-    
-}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     //self.tabBarController.tabBar.hidden = true;
-    [self.navigationController setNavigationBarHidden:NO];
+    
     self.imageUrlArray = [[NSMutableArray alloc]init];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(closeChooseBoardView) name:CloseChooseBoardNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showEditPlacePhotoView) name:PopUpEditPlacePhotoNotification object:nil];
     self.boardArray = [NSMutableArray array];
 	[self updateBoard];
-    self.title = [self.placeData objectForKey:@"name"];
+    //self.title = [self.placeData objectForKey:@"name"];
+    /*
     self.addressLabel = [[UILabel alloc]initWithFrame:CGRectMake(10,220,246,19)];
     self.addressLabel.textAlignment = NSTextAlignmentCenter;
     self.addressLabel.text = self.venue.location.address;
     self.addressLabel.font = [UIFont systemFontOfSize:15];
     [self.view addSubview:self.addressLabel];
     
-    
-    self.distanceLabel = [[UILabel alloc]initWithFrame:CGRectMake(10,244,246,20)];
-    self.distanceLabel.text = [NSString stringWithFormat:@"%@m", self.venue.location.distance];
-    self.distanceLabel.font = [UIFont systemFontOfSize:15];
-    self.distanceLabel.textColor = [UIColor lightGrayColor];
-    self.distanceLabel.textAlignment  = NSTextAlignmentCenter;
-    [self.view addSubview:self.distanceLabel];
-    
-    
-    self.mapButton = [[UIButton alloc]initWithFrame:CGRectMake(255,218,55,50)];
-    [self.mapButton setImage:[UIImage imageNamed:@"placepin.png"] forState:UIControlStateNormal];
-    self.mapButton.layer.borderWidth = 1.0;
-    self.mapButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    [self.mapButton addTarget:self action:@selector(showMap) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:self.mapButton];
-    
+    */
+
     self.chooseBoardView = [[MTChooseBoardViewController alloc]initWithImage:nil AndVenue:self.venue];
     self.chooseBoardView.view.frame = CGRectMake(0, 130, self.view.frame.size.width, self.view.frame.size.height);
     UIView *greyView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
@@ -91,75 +62,103 @@
     [[UIApplication sharedApplication].keyWindow addSubview:greyView];
     [[UIApplication sharedApplication].keyWindow addSubview:self.chooseBoardView.view];
     
-    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(10,218,246,50)];
-    imageView.layer.borderWidth = 1.0;
-    imageView.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    [self.view addSubview:imageView];
     
     
     //NSDictionary *categoryDict = [self.placeData objectForKey:@"catogories"];
     //add scroll view
-    if([[[UIDevice currentDevice]systemVersion]floatValue]>=7.0){
-        self.automaticallyAdjustsScrollViewInsets = NO;
-        self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height+10, 320, 180)];
-    }
-    else {
-        self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, 320, 180)];
+  
+    self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, 320, 180)];
         
-    }
+    
     
     [self.scrollView setPagingEnabled:YES];
     self.scrollView.showsHorizontalScrollIndicator =NO;
+    [self.view addSubview:self.scrollView];
+    
+   
+    self.table = [[UITableView alloc]initWithFrame:CGRectMake(0, 180, 320, 270) style:UITableViewStyleGrouped];
+    self.table.delegate = self;
+    self.table.dataSource = self;
+    [self.view addSubview:self.table];
+    
+    if ([self.table respondsToSelector:@selector(setSeparatorInset:)]) {
+        [self.table setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    UIButton *backButton =  [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    backButton.frame = CGRectMake(5, 10, 50, 20);
+    [backButton setTitle:@"back" forState:UIControlStateNormal];
+    [self.view addSubview:backButton];
+    [backButton addTarget:self action:@selector(navBack) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIBarButtonItem *pinItem = [[UIBarButtonItem alloc] initWithTitle:@"Pin" style:UIBarButtonItemStylePlain target:self action:@selector(pin:)];
+    self.navigationItem.rightBarButtonItem = pinItem;
+
+
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES];
+}
+
+-(void)navBack{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+-(void)initData{
+    
     NSDictionary *pictureDict = [self.placeData objectForKey:@"photos"];
     NSArray *array = [pictureDict objectForKey:@"groups"];
     if(array.count!=0){
-    pictureDict = [array objectAtIndex:0];
-    NSArray *picArray = [pictureDict objectForKey:@"items"];
-    if(picArray.count!=0){
-        for (int i=0; i<picArray.count; i++) {
-            
-            
-            NSDictionary *picDict = [picArray objectAtIndex:i];
-            NSString *str= [picDict objectForKey:@"prefix"];
-            str = [str stringByAppendingString:@"300x300"];
-            str = [str stringByAppendingString:[picDict objectForKey:@"suffix"]];
-            //NSLog(@"%@",str);
-            UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(i*320,0, 320, 160)] ;
-            [imgView setImageWithURL:[NSURL URLWithString:str]];
-            [self.scrollView addSubview:imgView];
-            [self.imageUrlArray addObject:str];
-           
+        pictureDict = [array objectAtIndex:0];
+        NSArray *picArray = [pictureDict objectForKey:@"items"];
+        if(picArray.count!=0){
+            for (int i=0; i<picArray.count; i++) {
+                
+                
+                NSDictionary *picDict = [picArray objectAtIndex:i];
+                NSString *str= [picDict objectForKey:@"prefix"];
+                str = [str stringByAppendingString:@"300x300"];
+                str = [str stringByAppendingString:[picDict objectForKey:@"suffix"]];
+                //NSLog(@"%@",str);
+                UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(i*320,0, 320, 160)] ;
+                [imgView setImageWithURL:[NSURL URLWithString:str]];
+                [self.scrollView addSubview:imgView];
+                [self.imageUrlArray addObject:str];
+                
+                
+            }
             
         }
-       
-    }
-    
-        self.scrollView.contentSize = CGSizeMake(picArray.count*320, 180) ;
+        
+        self.scrollView.contentSize = CGSizeMake(picArray.count*320, 160) ;
         self.scrollView.delegate = self;
-        [self.view addSubview:self.scrollView];
+        
+        UITapGestureRecognizer *sigleTapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickImage:)];
+        [self.scrollView addGestureRecognizer:sigleTapRecognizer];
+        //[self.view addSubview:self.scrollView];
         //add pageControl
         
         self.pageControl = [[UIPageControl alloc] init];
         
-        self.pageControl.frame = CGRectMake(150, 200, 20, 20);
+        self.pageControl.frame = CGRectMake(150, 140, 20, 20);
         self.pageControl.numberOfPages = picArray.count;
         
         self.pageControl.currentPage = 0;
         [self.pageControl addTarget:self action:@selector(changePage:)forControlEvents:UIControlEventValueChanged];
         
         [self.view addSubview:self.pageControl];
+        UILabel *numberLabel = [[UILabel alloc]initWithFrame:CGRectMake(280, 130, 30, 20)];
+        numberLabel.text = [NSString stringWithFormat:@"%d",self.imageUrlArray.count];
+        numberLabel.textColor = [UIColor blueColor];
+        [self.view addSubview:numberLabel];
+        
     }
-    
-    if ([self.table respondsToSelector:@selector(setSeparatorInset:)]) {
-        [self.table setSeparatorInset:UIEdgeInsetsZero];
-    }
-    
-    
-    
-    UIBarButtonItem *pinItem = [[UIBarButtonItem alloc] initWithTitle:@"Pin" style:UIBarButtonItemStylePlain target:self action:@selector(pin:)];
-    self.navigationItem.rightBarButtonItem = pinItem;
 
-
+    [self.table reloadData];
     
 }
 
@@ -170,6 +169,7 @@
     [self.navigationController pushViewController:viewController animated:YES];
     
 }
+
 - (void)scrollViewDidScroll:(UIScrollView *)sender {
     
     int page = self.scrollView.contentOffset.x / 310;
@@ -182,6 +182,13 @@
     int page = self.pageControl.currentPage;
     
     [self.scrollView setContentOffset:CGPointMake(320 * page, 0)];
+    
+}
+
+-(void)clickImage:(id)sender{
+    MTPlaceImageViewController *controller = [[MTPlaceImageViewController alloc]init];
+    controller.imageUrlArray = self.imageUrlArray;
+    [self.navigationController pushViewController:controller animated:YES];
     
 }
 
@@ -229,7 +236,8 @@
     viewController.imageStrArray = self.imageUrlArray;
     viewController.placeName = self.venue.name;
     viewController.location = self.venue.location.address;
-    [self.navigationController pushViewController:viewController animated:NO];}
+    [self.navigationController pushViewController:viewController animated:NO];
+}
 
 -(IBAction)pin:(id)sender {
     //to do
@@ -243,19 +251,12 @@
     //[self.navigationController pushViewController:controller animated:YES];
     
 }
-#pragma mark - segue
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.identifier isEqualToString:@"Map"]){
-        MTMapViewController *destViewController = segue.destinationViewController;
-        destViewController.venue = self.venue;
-    }
-}
+
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.boardArray.count;
+    return 2;
        
 }
 
@@ -265,21 +266,86 @@
     
     UITableViewCell *cell;
     
-    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
-    PFObject *mapObject = [self.boardArray objectAtIndex:indexPath.row];
-   
-    cell.textLabel.text = mapObject[Title];
-    cell.textLabel.font = [UIFont systemFontOfSize:12];
-    //cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    if(indexPath.section==0){
+        if(indexPath.row==0) {
+            NSString *markStr = @"/";
+            NSString *str = [NSString stringWithFormat:@"pin-s+48C(%f,%f)",self.venue.coordinate.longitude,self.venue.coordinate.latitude];
+            markStr = [markStr stringByAppendingString:str];
+            UIImageView *mapImgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 120)];
+            
+            NSString *urlStr = [NSString stringWithFormat:@"%@%@%@/%f,%f,10/%.0fx%.0f.png",MapBoxPictureAPI,MapId,markStr,self.venue.coordinate.longitude,self.venue.coordinate.latitude,mapImgView.frame.size.width,mapImgView.frame.size.height];
+            [mapImgView setImageWithURL:[NSURL URLWithString:urlStr]];
+            [cell.contentView addSubview:mapImgView];
+            
+        }
+        else{
+            cell.textLabel.text = self.venue.location.address;
         
-    
-    
+        }
+        
+    }
+    else{
+        if(indexPath.row==0) {
+            UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake(5, 5, 30, 30)];
+            [cell.contentView addSubview:imgView];
+            imgView.image = self.map.authorImage;
+            imgView.layer.masksToBounds = YES;
+            imgView.layer.cornerRadius =15;
+            [cell.contentView addSubview:imgView];
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(40, 5, 70, 20)];
+            label.text = @"Pinned by";
+            [cell.contentView addSubview:label];
+            label.font = [UIFont systemFontOfSize:11];
+            label.textColor = [UIColor lightGrayColor];
+            label = [[UILabel alloc]initWithFrame:CGRectMake(40, 15, 100, 20)];
+            label.text = [self.map.author objectForKey:@"username"];
+            [cell.contentView addSubview:label];
+        }
+        else{
+            
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(40, 5, 70, 20)];
+            label.text = @"Pinned in";
+            [cell.contentView addSubview:label];
+            label.font = [UIFont systemFontOfSize:11];
+            label.textColor = [UIColor lightGrayColor];
+            label = [[UILabel alloc]initWithFrame:CGRectMake(40, 15, 100, 20)];
+            label.text = [self.map.mapObject objectForKey:Title];
+            [cell.contentView addSubview:label];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            
+        }
+        
+    }
+   
     return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if(indexPath.section==0&indexPath.row==0)
+        return 120;
     return 40;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    if(self.map){
+        return 2;
+    }
+    return 1;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 1;
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    MTMapDetailViewController *controller = [[MTMapDetailViewController alloc]init];
+    controller.mapData = self.map;
+    controller.placeArray = self.mapPlaceArray;
+    [self.navigationController pushViewController:controller animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -290,8 +356,6 @@
 
 -(void)dealloc{
     UIView *greyView = (UIView *)[[UIApplication sharedApplication].keyWindow viewWithTag:101];
-
-   
     [[NSNotificationCenter defaultCenter] removeObserver:CloseChooseBoardNotification];
     [self.chooseBoardView.view removeFromSuperview];
     [greyView removeFromSuperview];
