8109064a0604faceac6f7e448efe0bace1aeb464
BlueFeather <Vivian@Vivians-Mac-mini.local>
2014-04-22 17:24:27 +0800

    modify the map boxAPI name

--- Maptube/Maptube/Defines.h
+++ Maptube/Maptube/Defines.h
@@ -32,10 +32,13 @@
 #define CollectMap @"CollectMap"
 #define Author @"Author"
 #define PlacePhotos @"placePhotos"
+#define City @"city"
 
 #define MapId @"bluefeather.hkh8138o"
-#define MapBoxPictureAPI @"http://api.tiles.mapbox.com/v3/"
+#define MapBoxAPI @"http://api.tiles.mapbox.com/v3/"
+
 //http://api.tiles.mapbox.com/v3/{mapid}/{markers}/{lon},{lat},{z}/{width}x{height}.{format}
+//http://api.tiles.mapbox.com/v3/{mapid}/geocode/{lon},{lat}.json
 //The optional {markers} parameter can include one or more markers in a comma-separated list. See the Markers section for options
 
 //markers: {name}-{label}+{color}({lon},{lat})
